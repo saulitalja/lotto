3fc71c6c6debc2136cf27b3610a92e0d8374a38f
saulitalja <60874991+saulitalja@users.noreply.github.com>
2023-12-02 16:48:41 +0200

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Lottogeneraattori {
+public class MainClass {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
